3880462060fa3eaaa13b8eeb2e32ae8a2096d5ee
viktorya_ <viktorya.mazmanyan@debifi.com>
2023-11-24 17:05:33 +0400

    fix calc

--- mock/info.js
+++ mock/info.js
@@ -53,59 +53,6 @@ const data = [
   },
 
   {
-    ownerID: 44444444,
-    mode: 1,
-    functionId: "1", // voda, wax, ...
-    updatedAt: "2023-03-22T10:39:52.652Z",
-    createdAt: "2023-03-22T10:39:52.652Z",
-  },
-
-  {
-    ownerID: 44444444,
-    mode: 2,
-
-    functionId: "7", // voda, wax, ...
-    updatedAt: "2023-03-22T10:39:52.652Z",
-    createdAt: "2023-03-22T10:39:52.652Z",
-  },
-
-  {
-    ownerID: 44444444,
-    mode: 3,
-
-    functionId: "0", // voda, wax, ...
-    updatedAt: "2023-03-22T10:39:52.652Z",
-    createdAt: "2023-03-22T10:39:52.652Z",
-  },
-
-  {
-    ownerID: 44444444,
-    mode: 4,
-
-    functionId: "12", // voda, wax, ...
-    updatedAt: "2023-03-22T10:39:52.652Z",
-    createdAt: "2023-03-22T10:39:52.652Z",
-  },
-
-  {
-    ownerID: 44444444,
-    mode: 5,
-
-    functionId: "13", // voda, wax, ...
-    updatedAt: "2023-03-22T10:39:52.652Z",
-    createdAt: "2023-03-22T10:39:52.652Z",
-  },
-
-  {
-    ownerID: 44444444,
-    mode: 6,
-
-    functionId: "18", // voda, wax, ...
-    updatedAt: "2023-03-22T10:39:52.652Z",
-    createdAt: "2023-03-22T10:39:52.652Z",
-  },
-  //1111111111111
-  {
     ownerID: 44444445,
     mode: 1,
     functionId: "1", // voda, wax, ...
