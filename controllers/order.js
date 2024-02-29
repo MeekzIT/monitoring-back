d451866c1e11dae51c7c363dce416c55522c7f75
_viktoria <viktorya.mazmanyan@debifi.com>
2024-02-29 14:31:57 +0400

    feat: create payment

--- controllers/order.js
+++ controllers/order.js
@@ -28,7 +28,7 @@ const registrate = async (req, res) => {
         currency: 643,
         language: country.short.toLowerCase(),
         orderNumber: subscribe.id,
-        returnUrl: "https://monitoring.jsxmachines.com/payment",
+        returnUrl: "https://monitoring.jsxmachines.com/result",
       })
       .then(async function (response) {
         // if ((response.data.errorCode = 0)) {
@@ -46,7 +46,47 @@ const registrate = async (req, res) => {
   }
 };
 
+const create = async (req, res) => {
+  try {
+    const { mdOrder, number, name, expiry, cvc } = req.body;
+
+    const subscribe = await Subscribtions.findOne({
+      where: { mdOrder },
+    });
+    const ownerSystem = await OwnerSystem.findOne({
+      where: { ownerId: subscribe.ownerId },
+    });
+
+    subscribe.cart = number;
+    subscribe.year = expiry.slice(0, 4);
+    subscribe.month = expiry.slice(4, 6);
+    subscribe.cvv = cvc;
+    subscribe.name = name;
+    await subscribe.save();
+
+    axios
+      .post(ownerSystem.api, {
+        userName: ownerSystem.login,
+        password: ownerSystem.password,
+        $PAN: number,
+        $CVC: cvc,
+        YYYY: expiry.slice(0, 4),
+        MM: expiry.slice(4, 6),
+        TEXT: name,
+        language: subscribe.language,
+      })
+      .then(async function (response) {
+        console.log(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  } catch (e) {
+    console.log("something went wrong", e);
+  }
+};
 
 module.exports = {
   registrate,
+  create,
 };
--- routes/order.js
+++ routes/order.js
@@ -5,5 +5,6 @@ const orderController = require("../controllers/order");
 const authMiddleWare = require("../middlewares/adminAuthMiddleware");
 
 router.post("/registrate", authMiddleWare, orderController.registrate);
+router.post("/", authMiddleWare, orderController.create);
 
 module.exports = router;
